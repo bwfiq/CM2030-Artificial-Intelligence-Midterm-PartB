a4d69e983c09a8e94311acee5119956b97762c35
Mohammad Rafiq <googaabumtum@gmail.com>
2024-07-08 02:16:12 +0800

    Added mountain environment to simulation.py

--- simulation.py
+++ simulation.py
@@ -1,4 +1,6 @@
 import pybullet as p
+import pybullet_data
+import numpy as np
 from multiprocessing import Pool
 
 class Simulation: 
@@ -6,59 +8,65 @@ class Simulation:
         self.physicsClientId = p.connect(p.DIRECT)
         self.sim_id = sim_id
 
-    def run_creature(self, cr, iterations=2400):
+    def run_creature(self, cr, iterations=2400):     
         pid = self.physicsClientId
+        p.setAdditionalSearchPath(pybullet_data.getDataPath(), physicsClientId=pid)
+        p.setAdditionalSearchPath('shapes/', physicsClientId=pid)
         p.resetSimulation(physicsClientId=pid)
         p.setPhysicsEngineParameter(enableFileCaching=0, physicsClientId=pid)
-
         p.setGravity(0, 0, -10, physicsClientId=pid)
-        plane_shape = p.createCollisionShape(p.GEOM_PLANE, physicsClientId=pid)
-        floor = p.createMultiBody(plane_shape, plane_shape, physicsClientId=pid)
-
-        xml_file = 'temp' + str(self.sim_id) + '.urdf'
-        xml_str = cr.to_xml()
-        with open(xml_file, 'w') as f:
-            f.write(xml_str)
         
+        # Create the environment
+        arena_size = 20
+        wall_height = 1
+        wall_thickness = 0.5
+        floor_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, arena_size/2, wall_thickness], physicsClientId=pid)
+        floor_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, arena_size/2, wall_thickness], rgbaColor=[1, 1, 0, 1], physicsClientId=pid)
+        floor_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=floor_collision_shape, baseVisualShapeIndex=floor_visual_shape, basePosition=[0, 0, -wall_thickness], physicsClientId=pid)
+        wall_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, wall_thickness/2, wall_height/2], physicsClientId=pid)
+        wall_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, wall_thickness/2, wall_height/2], rgbaColor=[0.7, 0.7, 0.7, 1], physicsClientId=pid)  # Gray walls
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[0, arena_size/2, wall_height/2], physicsClientId=pid)
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[0, -arena_size/2, wall_height/2], physicsClientId=pid)
+        wall_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[wall_thickness/2, arena_size/2, wall_height/2], physicsClientId=pid)
+        wall_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[wall_thickness/2, arena_size/2, wall_height/2], rgbaColor=[0.7, 0.7, 0.7, 1], physicsClientId=pid)  # Gray walls
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[arena_size/2, 0, wall_height/2], physicsClientId=pid)
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[-arena_size/2, 0, wall_height/2], physicsClientId=pid)
+        mountain_position = (0, 0, -1)
+        mountain_orientation = p.getQuaternionFromEuler((0, 0, 0))
+        mountain = p.loadURDF("gaussian_pyramid.urdf", mountain_position, mountain_orientation, useFixedBase=1, physicsClientId=pid)
+
+        # load in the creature
+        xml_file = 'temp' + str(self.sim_id) + '.urdf'
+        with open(xml_file, 'w') as f:
+            f.write(cr.to_xml())
         cid = p.loadURDF(xml_file, physicsClientId=pid)
+        p.resetBasePositionAndOrientation(cid, [5, 5, 2.5], [0, 0, 0, 1], physicsClientId=pid)
 
-        p.resetBasePositionAndOrientation(cid, [0, 0, 2.5], [0, 0, 0, 1], physicsClientId=pid)
-
-
+        # Run the simulation
         for step in range(iterations):
             p.stepSimulation(physicsClientId=pid)
+            
+            # Move the creature's motors
             if step % 24 == 0:
-                self.update_motors(cid=cid, cr=cr)
+                for jid in range(p.getNumJoints(cid, physicsClientId=self.physicsClientId)):
+                    m = cr.get_motors()[jid]
+                    p.setJointMotorControl2(cid, jid, 
+                            controlMode=p.VELOCITY_CONTROL, 
+                            targetVelocity=m.get_output(), 
+                            force = 5, 
+                            physicsClientId=self.physicsClientId)
+                    
+            # Update the creature's position
+            pos, _ = p.getBasePositionAndOrientation(cid, physicsClientId=pid)
+            # Kill the creature if it goes out of bounds
+            if pos[0] > arena_size/2 or pos[0] < -arena_size/2 or pos[1] > arena_size/2 or pos[1] < -arena_size/2:
+                cr.set_fitness(0)
+                break
+            else:
+                cr.update_position(pos)
+                cr.set_fitness(cr.get_distance_travelled())
 
-            pos, orn = p.getBasePositionAndOrientation(cid, physicsClientId=pid)
-            cr.update_position(pos)
-            #print(pos[2])
-            #print(cr.get_distance_travelled())
-        
-    
-    def update_motors(self, cid, cr):
-        """
-        cid is the id in the physics engine
-        cr is a creature object
-        """
-        for jid in range(p.getNumJoints(cid,
-                                        physicsClientId=self.physicsClientId)):
-            m = cr.get_motors()[jid]
-
-            p.setJointMotorControl2(cid, jid, 
-                    controlMode=p.VELOCITY_CONTROL, 
-                    targetVelocity=m.get_output(), 
-                    force = 5, 
-                    physicsClientId=self.physicsClientId)
-        
-
-    # You can add this to the Simulation class:
-    def eval_population(self, pop, iterations):
-        for cr in pop.creatures:
-            self.run_creature(cr, 2400) 
-
-
-class ThreadedSim():
+class ThreadedSim:
     def __init__(self, pool_size):
         self.sims = [Simulation(i) for i in range(pool_size)]
 
