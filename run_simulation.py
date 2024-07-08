842c32b051542b419133d96d20de18884c36315a
Mohammad Rafiq <googaabumtum@gmail.com>
2024-07-08 09:40:17 +0800

    Added param tweaking and graph generation

--- run_simulation.py
+++ run_simulation.py
@@ -1,63 +1,42 @@
-import numpy as np
-import genome
-import creature
-import population
 import simulation
-import time
+import matplotlib.pyplot as plt
+import os
 
-start_time = time.time()
-pop = population.Population(pop_size=10, gene_count=3)
-sim = simulation.ThreadedSim(pool_size=16)
+# Create the "results" subfolder if it doesn't exist
+if not os.path.exists("results"):
+    os.makedirs("results")
 
-# Mutation Parameters
-crossover_iterations = 1
-point_mutations = 1
-point_mutation_rate = 0.1
-shrink_mutations = 1
-shrink_mutation_rate = 0.25
-grow_mutations = 1
-grow_mutation_rate = 0.1
+# Create a dict for the graphs to reference
+overall_results = {
+    "Median Fitness": [],
+    "Standard Deviation of Fitness": [],
+    "Median Distance Travelled": [],
+    "Mutation Improvement Count": []
+}
 
-for iteration in range(100):
-    # Run the simulation and get the fitnesses
-    sim.eval_population(pop, 2400) # 10 seconds for each creature
-    fits = [cr.get_fitness() for cr in pop.creatures]
-    #print("fits", fits)
-    print(iteration, "fittest:", np.round(np.max(fits), 3), "mean:", np.round(np.mean(fits), 3))
-    
-    # Genetic algorithm 
-    # Find the two best parents
-    new_creatures = []
-    p1 = pop.creatures[np.argmax(fits)]
-    fits[np.argmax(fits)] = 0 # remove the best parent from the list
-    p2 = pop.creatures[np.argmax(fits)]
-    for i in range(len(pop.creatures)):
-        for _ in range(crossover_iterations):
-            dna = genome.Genome.crossover(p1.dna, p2.dna)
-        for _ in range(point_mutations):
-            dna = genome.Genome.point_mutate(dna, rate=point_mutation_rate)
-        for _ in range(shrink_mutations):
-            dna = genome.Genome.shrink_mutate(dna, rate=shrink_mutation_rate)
-        for _ in range(grow_mutations):
-            dna = genome.Genome.grow_mutate(dna, rate=grow_mutation_rate)
-        cr = creature.Creature(1)
-        cr.update_dna(dna)
-        new_creatures.append(cr)
-    
-    # Replace the first two creatures with the parents
-    new_cr1 = creature.Creature(1)
-    new_cr1.update_dna(p1.dna)
-    new_creatures[0] = new_cr1
-    new_cr2 = creature.Creature(1)
-    new_cr2.update_dna(p2.dna)
-    new_creatures[1] = new_cr2
-    
-    # Get the best creature from each generation and save it to file
-    filename = "elite_" + str(iteration) + ".csv"
-    genome.Genome.to_csv(p1.dna, filename)
-    #print ("Elite saved to", filename, "with fitness", p1.get_fitness())
-    
-    # Get the new creatures ready for next simulation
-    pop.creatures = new_creatures
-end_time = time.time()
-print("Time taken:", end_time - start_time)
+# Run the simulations and store the results
+for simulation_count in range(5):
+    print("--- Simulation", simulation_count, "---")
+    results = simulation.RunSimulation.run_simulation(population_size=10, 
+                                                      gene_count=3,
+                                                      num_generations=100, 
+                                                      crossover_iterations=1,
+                                                      point_mutations=1,
+                                                      point_mutation_rate=0.1,
+                                                      shrink_mutations=1,
+                                                      shrink_mutation_rate=0.25,
+                                                      grow_mutations=1,
+                                                      grow_mutation_rate=0.1)
+    for key in results.keys():
+        overall_results[key].append(results[key])
+
+# Plot the median fitness over the generations and simulations and save to file
+for key, value in overall_results.items():
+    x = list(range(len(overall_results[key][0])))
+    for i in range(len(overall_results[key])):
+        plt.plot(x, value[i], label="Simulation " + str(i+1))
+    plt.xlabel("Generation")
+    plt.ylabel(key)
+    plt.legend()
+    plt.savefig("results/" + key + ".png")
+    plt.clf()
