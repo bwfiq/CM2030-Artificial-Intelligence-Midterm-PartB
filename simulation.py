7c44e41bd3746dd26b65722a0d91f130cfb47429
Mohammad Rafiq <googaabumtum@gmail.com>
2024-07-08 09:40:05 +0800

    Moved simulation code to simulation.py

--- simulation.py
+++ simulation.py
@@ -1,7 +1,11 @@
+import time
 import pybullet as p
 import pybullet_data
 import numpy as np
 import os
+import creature
+import genome
+import population
 from multiprocessing import Pool
 
 class Simulation: 
@@ -112,3 +116,100 @@ class ThreadedSim:
                 # self.creatures array
                 new_creatures.extend(creatures)
         pop.creatures = new_creatures
+
+class RunSimulation():
+    @staticmethod
+    def run_simulation(population_size = 10, 
+                       gene_count = 3,
+                       num_generations=10, 
+                       crossover_iterations = 1,
+                       point_mutations = 1,
+                       point_mutation_rate = 0.5,
+                       shrink_mutations = 1,
+                       shrink_mutation_rate = 0.25,
+                       grow_mutations = 1,
+                       grow_mutation_rate = 0.5):
+        start_time = time.time()
+        pop = population.Population(pop_size=population_size, gene_count=gene_count)
+        sim = ThreadedSim(pool_size=16)
+
+        # Mutation Parameters
+        # params = {
+        #     "crossover_iterations": crossover_iterations,
+        #     "point_mutations": point_mutations,
+        #     "point_mutation_rate": point_mutation_rate,
+        #     "shrink_mutations": shrink_mutations,
+        #     "shrink_mutation_rate": shrink_mutation_rate,
+        #     "grow_mutations": grow_mutations,
+        #     "grow_mutation_rate": grow_mutation_rate
+        # }
+
+        # Metrics
+        results = {
+            "Median Fitness": [],
+            "Standard Deviation of Fitness": [],
+            "Median Distance Travelled": [],
+            "Mutation Improvement Count": []
+        }
+
+        for iteration in range(num_generations):
+            sim.eval_population(pop, 2400) # 10 seconds for each creature
+            fits = [cr.get_fitness() for cr in pop.creatures]
+            #print(iteration, "fittest:", np.round(np.max(fits), 3), "mean:", np.round(np.mean(fits), 3))
+            
+            # Metrics
+            total_distance_travelled = 0
+            mutation_improvement_count = 0
+            # Find the two best parents
+            new_creatures = []
+            p1 = pop.creatures[np.argmax(fits)]
+            fits[np.argmax(fits)] = 0 # remove the best parent from the list
+            p2 = pop.creatures[np.argmax(fits)]
+            for i in range(len(pop.creatures)):
+                # Metrics
+                total_distance_travelled += pop.creatures[i].get_distance_travelled() # for metrics
+                if iteration > 0 and pop.creatures[i].get_fitness() > results["Median Fitness"][iteration - 1]:
+                    mutation_improvement_count += 1
+                
+                # Mutations
+                dna = None
+                for _ in range(crossover_iterations):
+                    dna = genome.Genome.crossover(p1.dna, p2.dna)
+                for _ in range(point_mutations):
+                    dna = genome.Genome.point_mutate(dna, rate=point_mutation_rate)
+                for _ in range(shrink_mutations):
+                    dna = genome.Genome.shrink_mutate(dna, rate=shrink_mutation_rate)
+                for _ in range(grow_mutations):
+                    dna = genome.Genome.grow_mutate(dna, rate=grow_mutation_rate)
+                    
+                # Create a new creature with the new DNA
+                cr = creature.Creature(1)
+                cr.update_dna(dna)
+                new_creatures.append(cr)
+            
+            # Replace the first two creatures with the parents
+            new_cr1 = creature.Creature(1)
+            new_cr1.update_dna(p1.dna)
+            new_creatures[0] = new_cr1
+            new_cr2 = creature.Creature(1)
+            new_cr2.update_dna(p2.dna)
+            new_creatures[1] = new_cr2
+            
+            # Get the best creature from each generation and save it to file
+            filename = "elite_" + str(iteration) + ".csv"
+            genome.Genome.to_csv(p1.dna, filename)
+            #print ("Elite saved to", filename, "with fitness", p1.get_fitness())
+            
+            # Measure metrics
+            results["Median Fitness"].append(np.median(fits))
+            results["Standard Deviation of Fitness"].append(np.std(fits))
+            results["Median Distance Travelled"].append(total_distance_travelled / len(pop.creatures))
+            results["Mutation Improvement Count"].append(mutation_improvement_count)
+            
+            # Get the new creatures ready for next simulation
+            pop.creatures = new_creatures
+            
+        end_time = time.time()
+        print("Time taken:", end_time - start_time)
+        
+        return results
